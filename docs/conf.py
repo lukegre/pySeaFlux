7d44d44b86d96f2cd3595e8d50b637b9431fda31
Luke Gregor <2463416+lukegre@users.noreply.github.com>
2021-04-01 23:24:27 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ from pkg_resources import get_distribution
 root = pathlib.Path(__file__).parent.parent.absolute()
 os.environ["PYTHONPATH"] = str(root)
 sys.path.insert(0, "../..")
+sys.path.insert(1, "..")
 print("python exec:", sys.executable)
 print("sys.path:", sys.path)
 
