81c8497ed4e4df2d056383f1d884d1d038fc78e0
Luke Gregor <lukegre@gmail.com>
2021-04-01 20:34:05 +0200

    updated files

---
+++ README.md
@@ -0,0 +1,13 @@
+SeaFlux
+==============================
+[![Build Status](https://github.com/lukegre/seaflux/workflows/Tests/badge.svg)](https://github.com/lukegre/seaflux/actions)
+[![codecov](https://codecov.io/gh/lukegre/seaflux/branch/master/graph/badge.svg)](https://codecov.io/gh/lukegre/seaflux)
+[![License:MIT](https://img.shields.io/badge/License-MIT-lightgray.svg?style=flt-square)](https://opensource.org/licenses/MIT)[![pypi](https://img.shields.io/pypi/v/seaflux.svg)](https://pypi.org/project/seaflux)
+<!-- [![conda-forge](https://img.shields.io/conda/dn/conda-forge/seaflux?label=conda-forge)](https://anaconda.org/conda-forge/seaflux) -->[![Documentation Status](https://readthedocs.org/projects/seaflux/badge/?version=latest)](https://seaflux.readthedocs.io/en/latest/?badge=latest)
+
+
+Calculation of air-sea fluxes
+
+--------
+
+<p><small>Project based on the <a target="_blank" href="https://github.com/jbusecke/cookiecutter-science-project">cookiecutter science project template</a>.</small></p>
--- setup.py
+++ setup.py
@@ -1,44 +1,10 @@
-from os.path import exists
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-if exists("README.rst"):
-    with open("README.rst") as f:
-        long_description = f.read()
-else:
-    long_description = ""
-
-CLASSIFIERS = [
-    "Development Status :: 3 - Alpha",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Intended Audience :: Science/Research",
-    "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-]
 
 setup(
-    name="SeaFlux",
-    author="Luke Gregor",
-    author_email="lukegre@gmail.com	",
-    description="Calculate sea fluxes",
-    keywords="SeaFlux",
-    license="GNUv3",
-    classifiers=CLASSIFIERS,
-    url="https://github.com/luke-gregor/SeaFlux",
-    use_scm_version={"version_scheme": "guess-next-dev", "local_scheme": "dirty-tag"},
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    packages=find_packages(),
-    install_requires=["numpy", "pandas", "xarray", "pooch"],
-    test_suite="tests",
-    tests_require=["pytest-cov"],
-    setup_requires=[
-        "wheel",
-        "setuptools_scm",
-        "setuptools>=30.3.0",
-        "setuptools_scm_git_archive",
-    ],
+    use_scm_version={
+        "write_to": "seaflux/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    }
 )
