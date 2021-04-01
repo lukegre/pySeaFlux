e167241f6e6329bbfd65fab0e4c98f6aed5b82de
Luke Gregor <lukegre@gmail.com>
2021-04-01 22:49:21 +0200

    Squashed commit of the following:
    commit 44218854a3fe4705689123b8354df167018160ed
    Author: Luke Gregor <2463416+lukegre@users.noreply.github.com>
    Date:   Thu Apr 1 21:46:25 2021 +0200
    
        Migrate to julius' cookiecutter boilerplate (#15)
    
        * update precommit
    
        * updated files
    
        * updated environment files
    
        * added pyco2sys
    
        * don't need notebook folder
    
        * copied old readme to new one
    
        * Update README.md
    
        * Rename readthedocs.yml to .readthedocs.yml
    
        * Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ project = "seaflux"
 copyright = "2021, Luke Gregor"
 author = "Luke Gregor"
 
-release = ""  # get_distribution("seaflux").version
+release = get_distribution("seaflux").version
 # for example take major/minor
 version = ".".join(release.split(".")[:2])
 
