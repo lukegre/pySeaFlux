d52f025639f3e56902ae1fdf9749618807bfa08b
Luke Gregor <lukegre@gmail.com>
2021-04-02 18:02:37 +0200

    cleaned up complicated helper functions

--- seaflux/auxiliary_equations.py
+++ seaflux/auxiliary_equations.py
@@ -1,25 +1,28 @@
+"""
+Additional equations
+--------------------
+
+Not necessarily linked to the marine carbonate system, but are 
+useful. 
+"""
+
 from . import check_units as check
+import numpy as np
 
 
 def pressure_height_correction(pres_hPa, tempSW_C, sensor_height=10.0):
-    """
-    Returns exact sea level pressure if the sensor is measuring at height
+    """Returns exact sea level pressure if the sensor is measuring at height
 
-    Parameters
-    ----------
-    pres_hPa : np.array
-        Pressure in kiloPascal measured at height
-    temp_C : np.array
-        Temperature of the seawater in deg C
-    sensor_height : float
-        the height of the sensor above sea level. Can be negative if you want
-        to convert SLP to sensor height pressure
+    Args:
+        pres_hPa (array): Pressure in kiloPascal measured at height
+        tempSW_C (array): Temperature of the seawater in deg C
+        sensor_height (float, optional): the height of the sensor above sea 
+            level. Can be negative if you want to convert SLP to sensor height
+            pressure. Defaults to 10.0.
 
-    Return
-    ------
-    presCor_kPa : np.array
-        height corrected pressure
-    """
+    Returns:
+        array: height corrected pressure
+    """    
 
     T = check.temp_K(tempSW_C + 273.15)  # temperature in Kelvin
     P = check.pres_atm(pres_hPa / 1013.25)  # pressure in Pascal
@@ -39,38 +42,33 @@ def pressure_height_correction(pres_hPa, tempSW_C, sensor_height=10.0):
 
 
 def temperature_correction(temp_in, temp_out):
-    """
+    """pCO2 correction factor for temperature changes
+
     Calculate a correction factor for the temperature difference between the
     intake and equilibrator. This is based on the empirical relationship used
     in Takahashi et al. 1993.
-    pCO2_Tout = pCO2_Tin * T_factor
 
-    Parameters
-    ----------
-    temp_in : np.array
-        temperature at which original pCO2 is measured
-    temp_out : np.array
-        temperature for which pCO2 should be represented
+    .. math::
 
-    Return
-    ------
-    factor : np.array
-        a correction factor to be multiplied to pCO2 (unitless)
+        pCO_2^{Tout} = pCO_2^{Tin} * T^{factor}
 
-    References
-    ----------
-    Takahashi, Taro et al. (1993). Seasonal variation of CO2 and nutrients in
+    Args:
+        temp_in (array): temperature at which original pCO2 is measured
+        temp_out (array): temperature for which pCO2 should be represented
+
+    Returns:
+        array: a correction factor to be multiplied to pCO2 (unitless)
+
+    References:
+        Takahashi, Taro et al. (1993). Seasonal variation of CO2 and nutrients in
         the high-latitude surface oceans: A comparative study. Global
         Biogeochemical Cycles, 7(4), 843–878. https://doi.org/10.1029/93GB02263
     """
-
-    from numpy import array, exp
-
     # see the Takahashi 1993 paper for full description
 
-    Ti = array(temp_in)
-    To = array(temp_out)
+    Ti = np.array(temp_in)
+    To = np.array(temp_out)
 
-    factor = exp(0.0433 * (To - Ti) - 4.35e-05 * (To ** 2 - Ti ** 2))
+    factor = np.exp(0.0433 * (To - Ti) - 4.35e-05 * (To ** 2 - Ti ** 2))
 
     return factor
