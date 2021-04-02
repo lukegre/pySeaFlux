2afe6a1a2d5775b0911e37739026606e17d38e2a
Luke Gregor <lukegre@gmail.com>
2021-04-02 18:55:16 +0200

    removed woolf test

--- tests/test_fluxes.py
+++ tests/test_fluxes.py
@@ -7,12 +7,3 @@ def test_CO2flux_bulk():
 
     # seaward is negative
     assert flux < 0
-
-
-def test_CO2flux_woolf():
-    from seaflux import flux_calculations as flux
-
-    flux = flux.flux_woolf2016_rapid(25, 35, 300, 400, 1013.25, 20)
-
-    # seaward is negative
-    assert flux < 0
