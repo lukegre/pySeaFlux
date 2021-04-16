fedf9f6f217364aa1ec57904f5262187c749ffad
Luke Gregor <lukegre@gmail.com>
2021-04-16 11:06:18 +0200

    yaml file was not included in pypi seup

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 
 setup(
+    include_package_data=True,
     use_scm_version={
         "write_to": "seaflux/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
+    },
 )
