ed6f013f15d4c0c1666959284661dde4417cad75
lukegre <lukegre@gmail.com>
2021-10-08 12:28:07 +0200

    data not imported by default. Updated zenodo links

--- pyseaflux/__init__.py
+++ pyseaflux/__init__.py
@@ -5,7 +5,5 @@ from . import auxiliary_equations as eqs
 from . import gas_transfer_velocity as kw
 from . import vapour_pressure
 from .area import get_area_from_dataset
-from .data.download_zenodo_files import get_seaflux_data, get_zenodo_catalog
-from .data.utils import preprocess as preprocess
 from .fco2_pco2_conversion import fCO2_to_pCO2, pCO2_to_fCO2
 from .flux_calculations import flux_bulk
--- pyseaflux/data/__init__.py
+++ pyseaflux/data/__init__.py
@@ -1,3 +1,10 @@
 """
-All modules and functions related to the SeaFlux data set.
+This module is only intended to be by the SeaFlux authors to
+download the data required to create the SeaFlux ensemble.
+Has links to most data sources (ERA5 might not be included)
+
+Hence, this module is not imported by default and submodules
+should be imported on demand.
 """
+
+from .download_zenodo_files import get_seaflux_data, get_zenodo_catalog
